fed2423e8c0374d192b4b9c20779288f9756780a
Mauri de Souza Nunes <mauri870@gmail.com>
2015-09-15 14:27:41 -0300

    Template updated

--- Http/Controllers/AdminController.php
+++ Http/Controllers/AdminController.php
@@ -1,12 +1,24 @@
-<?php namespace Modules\Admin\Http\Controllers;
+<?php
+namespace Modules\Admin\Http\Controllers;
 
 use Pingpong\Modules\Routing\Controller;
 
+use Prologue\Alerts\Facades\Alert;
+
 class AdminController extends Controller {
+
+	private $app;
+
+	public function __construct(){
+		$this->app = [
+			'name'=> env('APP_NAME',null),
+			'version'=> env('APP_VERSION',null)
+		];
+	}
 	
 	public function index()
 	{
-		return view('admin::index');
+		return view('admin::index')->with('app',$this->app);
 	}
 	
 }
